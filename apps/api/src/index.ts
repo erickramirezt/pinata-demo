d46b629d87d3ace170710a73f3d498f1884af41c
Erick Ramirez <erickramirez.dnbk@gmail.com>
2026-01-29 21:45:41 -0300

    feat: :sparkles: implement presigned url endpoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,38 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { PinataSDK } from 'pinata'
 
 const app = new Hono()
+
+app.use(cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+app.get('/presigned_url', async (c) => {
+  // Handle Auth
+
+  const pinata = new PinataSDK({
+    pinataJwt: String(process.env.PINATA_JWT),
+    pinataGateway: String(process.env.GATEWAY_URL),
+  })
+
+  const url = await pinata.upload.public.createSignedURL({
+    expires: 60, // Last for 60 seconds
+  })
+
+  return c.json({ url }, { status: 200 })
 })
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 4000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  },
+)
